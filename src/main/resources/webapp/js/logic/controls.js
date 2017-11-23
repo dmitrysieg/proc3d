bfbd842b683e0e20e4442267e4a3013dda973715
d.boldyrev <dmitry.sieg@gmail.com>
2017-11-23 16:21:47 +0200

    Decorated the graph

--- src/main/resources/webapp/js/logic/controls.js
+++ src/main/resources/webapp/js/logic/controls.js
@@ -50,24 +50,38 @@ define(function() {
     };
 
     UIGraph.prototype = {
+        colors: {
+            graph: {r: 0, g: 0, b: 0, a: 255},
+            bottom: {r: 30, g: 144, b: 255, a: 180},
+            top: {r: 30, g: 144, b: 255, a: 45}
+        },
+        toStyle: function(color) {
+            return 'rgba(' + color.r + ',' + color.g + ',' + color.b + ',' + color.a / 255.0 + ')';
+        },
         append: function(element) {
             this.canvas = document.createElement("canvas");
             this.canvas.width = 250;
             this.canvas.height = 250;
             element.appendChild(this.canvas);
+
+            // set clear color
+            var ctx = this.canvas.getContext("2d");
+            ctx.fillStyle = this.toStyle(this.colors.top);
+            ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
             return this;
         },
-        drawPoint: function(imageData, x, y) {
+        drawPoint: function(imageData, x, y, color) {
             var w = this.canvas.width;
             var index = 4 * (y * w + x);
-            imageData[index] = 0;
-            imageData[index + 1] = 0;
-            imageData[index + 2] = 0;
-            imageData[index + 3] = 255;
+            imageData[index] = color.r;
+            imageData[index + 1] = color.g;
+            imageData[index + 2] = color.b;
+            imageData[index + 3] = color.a;
         },
-        drawVLine: function(imageData, x, y1, y2) {
+        drawVLine: function(imageData, x, y1, y2, color) {
             if (y1 == y2) {
-                this.drawPoint(imageData, x, y1);
+                this.drawPoint(imageData, x, y1, this.colors.graph);
                 return;
             }
             var dy = y2 > y1 ? 1 : -1;
@@ -75,10 +89,10 @@ define(function() {
             var w = this.canvas.width;
             for (var y = y1; y != y2; y += dy) {
                 var index = 4 * (y * w + x);
-                imageData[index] = 0;
-                imageData[index + 1] = 0;
-                imageData[index + 2] = 0;
-                imageData[index + 3] = 255;
+                imageData[index] = color.r;
+                imageData[index + 1] = color.g;
+                imageData[index + 2] = color.b;
+                imageData[index + 3] = color.a;
             }
         },
         /**
@@ -92,10 +106,22 @@ define(function() {
             var y = Math.round(this.canvas.height * (1.0 - value));
             if (this.prevY == null) {
                 // draw a point
-                this.drawPoint(imgData.data, x, y);
+                this.drawPoint(imgData.data, x, y, this.colors.graph);
             } else {
                 // todo: rough vertical line to interpolate so far
-                this.drawVLine(imgData.data, x, this.prevY, y);
+                this.drawVLine(imgData.data, x, this.prevY, y, this.colors.graph);
+
+                // bottom fill
+                var max = this.prevY > y ? this.prevY : y;
+                if (max + 1 < this.canvas.height) {
+                    this.drawVLine(imgData.data, x, max + 1, this.canvas.height, this.colors.bottom);
+                }
+
+                // top fill
+                var min = this.prevY < y ? this.prevY : y;
+                if (min - 1 > 0) {
+                    this.drawVLine(imgData.data, x, 0, min - 1, this.colors.top);
+                }
             }
             this.prevY = y;
             ctx.putImageData(imgData, -1, 0);
